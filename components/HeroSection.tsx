cbd83d004163102d7a326e0e51ec090dd75abd23
aimane <120175452+rhiminiaimane@users.noreply.github.com>
2025-06-03 21:09:51 +0100

    Remove unused Link to fix build errors

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import Image from 'next/image';
 import styles from './Hero.module.css';
 import googlereviews from '../public/google-reviews.png';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import AboutSection from '@/components/AboutSection';
 import HeroSection from '@/components/HeroSection';
 import ServicesSection from '@/components/ServicesSection';
-import Link from 'next/link';
 import React from 'react';
 import Footer from '@/components/Footer';
 
